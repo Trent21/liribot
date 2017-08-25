a9003d01e697a6118de61245c4d3159edad8d048
Trent21 <tmitch.wall@gmail.com>
2017-08-25 09:51:47 -0400

    Twitter keys

---
+++ keys.js
@@ -0,0 +1,11 @@
+exports.twitterKeys = {
+  consumer_key: 'm1Szi1e1wcrlowTS07BT1XUBY',
+  consumer_secret: 	'I5HcrrwqZDU7na74gDFs9NVUKr1p60gQLacXj8q9C2CVX8Jg71',
+  access_token_key: '869936418294050817-EutWmP3u9OQegHJpTIhIwbJXicLecwQ',
+  access_token_secret: '08QtJ16h8kfXOgeTveYBSBZfl7d2QN8mOryCMZNc82ki4',
+}
+
+// exports.spotifyKeys = {
+//     id: '6613d93f82fd4d7b9a2b97ecc1e55f2f',
+//     secret: '2a3358fb308b4682910e07d036a070be'
+// }
